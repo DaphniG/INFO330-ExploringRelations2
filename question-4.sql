17cef3ee54b0d48654f2167186e37935d68d6d2c
DaphniG <114948275+DaphniG@users.noreply.github.com>
2023-04-11 19:18:05 -0700

    Update question-4.sql

--- question-4.sql
+++ question-4.sql
@@ -1,3 +1,3 @@
 -- Which sales agent made the most in sales in 2010?
 
-select employees.FirstName, employees.LastName, sum(invoices.total)  from employees inner join customers on employees.EmployeeID  = customers.SupportRepID inner join invoices on customers.CustomerID = invoices.CustomerID group by employees.FirstName;
+select employees.FirstName, employees.LastName, sum(invoices.total)  from employees inner join customers on employees.EmployeeID  = customers.SupportRepID inner join invoices on customers.CustomerID = invoices.CustomerID WHERE invoices.InvoiceDate BETWEEN '2010-01-01' AND '2010-12-31' group by employees.FirstName;
