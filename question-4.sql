76fd186c14f2155f9b4caf95b2161d5e14075752
DaphniG <114948275+DaphniG@users.noreply.github.com>
2023-04-11 18:30:51 -0700

    Update question-4.sql

--- question-4.sql
+++ question-4.sql
@@ -1,1 +1,3 @@
 -- Which sales agent made the most in sales in 2010?
+
+select employees.FirstName, employees.LastName, sum(invoices.total)  from employees inner join customers on employees.EmployeeID  = customers.SupportRepID inner join invoices on customers.CustomerID = invoices.CustomerID group by employees.FirstName;
