7c1f4e6f4c87248d828f434d6bb429cbf7b41780
DaphniG <114948275+DaphniG@users.noreply.github.com>
2023-04-11 19:56:22 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -1,1 +1,4 @@
 -- What was the most purchased track of 2013?
+
+select tracks.Name, sum(invoice_items.quantity) from tracks join invoice_items on tracks.TrackID = invoice_items.TrackID join invoices on invoice_items.InvoiceID = invoices.InvoiceID WHERE invoices.InvoiceDate BETWEEN '2013-01-01' AND '2013-12-31'
+GROUP BY tracks.TrackId;
