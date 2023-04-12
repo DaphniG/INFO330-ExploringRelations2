218a451f46ba773bba47d02c4981bd1985d7b4d8
DaphniG <114948275+DaphniG@users.noreply.github.com>
2023-04-11 23:10:35 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -1,4 +1,3 @@
 -- What was the most purchased track of 2013?
 
-select tracks.Name, sum(invoice_items.quantity) from tracks join invoice_items on tracks.TrackID = invoice_items.TrackID join invoices on invoice_items.InvoiceID = invoices.InvoiceID WHERE invoices.InvoiceDate BETWEEN '2013-01-01' AND '2013-12-31'
-GROUP BY tracks.TrackId;
+select tracks.Name, sum(invoice_items.quantity) from tracks join invoice_items on tracks.TrackID = invoice_items.TrackID join invoices on invoice_items.InvoiceID = invoices.InvoiceID WHERE invoices.InvoiceDate BETWEEN '2013-01-01' AND '2013-12-31' GROUP BY tracks.TrackId order by sum(invoice_items.quantity) desc;
